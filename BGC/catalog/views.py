e41f8b38ab42e318218d121cbb6dc0b0190294b6
Rafał <rawrqwe10@gmail.com>
2026-06-13 19:44:21 +0200

    add game_create and add_review

--- BGC/catalog/views.py
+++ BGC/catalog/views.py
@@ -1,19 +1,46 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .forms import BoardGameForm, ReviewForm
 from .models import BoardGame, Review
 
 
 def game_list(request):
     games = BoardGame.objects.all()
-    return render(request, "catalog/game_list.html",{"games": games})
+    return render(request, "catalog/game_list.html", {"games": games})
 
 
 def game_detail(request, pk):
     game = get_object_or_404(BoardGame, pk=pk)
     reviews = Review.objects.filter(game=game)
-    return render(request, "catalog/game_deatail.html",{
+    return render(request, "catalog/game_detail.html", {
         "game": game,
         "reviews": reviews
     })
 
+
 def game_create(request):
-    if request.method == "POST"
+    if request.method == "POST":
+        form = BoardGameForm(request.POST, request.FILES)
+        if form.is_valid():
+            game = form.save()
+            return redirect('game_detail', pk=game.pk)
+    else:
+        form = BoardGameForm()
+    return render(request, 'catalog/game_form.html', {'form': form})
+
+
+def add_review(request, pk):
+    game = get_object_or_404(BoardGame, pk=pk)
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)  # nie zapisuj jeszcze
+            review.game = game  # przypisz grę do recenzji
+            review.save()  # teraz zapisz
+            return redirect('game_detail', pk=game.pk)
+    else:
+        form = ReviewForm()
+    return render(request, 'catalog/review_form.html', {
+        'form': form,
+        'game': game,
+    })
