e88bf89963ab5e93bf65f89559353b099aa7f019
Rafał <rawrqwe10@gmail.com>
2026-06-15 19:44:34 +0200

    added translations of pools into Polish

--- BGC/catalog/forms.py
+++ BGC/catalog/forms.py
@@ -5,6 +5,19 @@ class BoardGameForm(forms.ModelForm):
     class Meta:
         model = BoardGame
         exclude = ['id'] #pole które nie chce żeby było widać
+        labels = {
+            'title': 'Tytuł',
+            'release_year': 'Rok wydania',
+            'publisher': 'Wydawca',
+            'description': 'Opis',
+            'image': 'Okładka',
+            'min_players': 'Minimalna liczba graczy',
+            'max_players': 'Maksymalna liczba graczy',
+            'min_time': 'Minimalny czas gry (minuty)',
+            'max_time': 'Maksymalny czas gry (minuty)',
+            'min_age': 'Minimalny wiek',
+            'genres': 'Gatunki',
+        }
 
 class ReviewForm(forms.ModelForm):
     class Meta:
@@ -13,3 +26,7 @@ class ReviewForm(forms.ModelForm):
             'score',
             'text',
         ]
+        labels = {
+            'score': 'Ocena',
+            'text': 'Recenzja',
+        }
