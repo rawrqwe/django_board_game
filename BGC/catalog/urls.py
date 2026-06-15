3ebba702dad1b888cc5d1673f1dcccef2cbbc5b5
Rafał <rawrqwe10@gmail.com>
2026-06-15 20:17:19 +0200

    Add game_main route and update related code

--- BGC/catalog/urls.py
+++ BGC/catalog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.game_list, name='game_list'),
+    path('', views.game_main, name='game_main'),
+    path('game_list/', views.game_list, name='game_list'),
     path('<int:pk>/', views.game_detail, name='game_detail'),
     path('create/', views.game_create, name='game_create'),
-    path('<int:pk>/review/', views.add_review,name='add_review'),
+    path('<int:pk>/review/', views.add_review, name='add_review'),
 
 ]
