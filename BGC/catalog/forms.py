7c1e600af7b249e81b35e46353b89525df11b9af
Rafał <rawrqwe10@gmail.com>
2026-06-15 19:27:52 +0200

    Fix BoardGameForm and ReviewForm inheritance

--- BGC/catalog/forms.py
+++ BGC/catalog/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 from .models import BoardGame, Review
 
-class BoardGameForm(forms.BaseForm):
+class BoardGameForm(forms.ModelForm):
     class Meta:
         model = BoardGame
         exclude = ['id'] #pole które nie chce żeby było widać
 
-class ReviewForm(forms.BaseForm):
+class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = [
