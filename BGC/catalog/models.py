32c44a061be69cdb2116375ad88fd594e100675b
Rafał <rawrqwe10@gmail.com>
2026-06-22 20:11:40 +0200

    Add validators to BoardGame model fields

--- BGC/catalog/models.py
+++ BGC/catalog/models.py
@@ -1,5 +1,7 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
-from django import forms
+
+from django.core.exceptions import ValidationError
 
 
 class Genre(models.Model):
@@ -13,16 +15,27 @@ class BoardGame(models.Model):
     title = models.CharField(max_length=200)
     release_year = models.IntegerField()
     publisher = models.CharField(max_length=200)
-    description = models.TextField(blank=True,)
+    description = models.TextField(blank=True)
     image = models.ImageField(upload_to='games/', blank=True)
 
-    min_players = models.IntegerField()
-    max_players = models.IntegerField()
-    min_time = models.IntegerField()
-    max_time = models.IntegerField()
-    min_age = models.IntegerField()
+    min_players = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
+    max_players = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
+    min_time = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    max_time = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    min_age = models.IntegerField(validators=[MinValueValidator(3), MaxValueValidator(18)])
 
     genres = models.ManyToManyField(Genre)
+
+    def clean(self):
+        if self.min_players > self.max_players:
+            raise ValidationError({
+                "min_players": "Minimalna liczba graczy nie może być większa od maksymalnej."
+            })
+
+        if self.min_time > self.max_time:
+            raise ValidationError({
+                "min_time": "Minimalny czas gry nie może być większy od maksymalnego."
+            })
 
     def __str__(self):
         return self.title
@@ -35,4 +48,3 @@ class Review(models.Model):
 
     def __str__(self):
         return f"{self.game.title} - {self.score}"
-
